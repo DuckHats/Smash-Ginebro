80c57f4c460404cafac443c5a8a1060628610aec
arnau clavijos roger <ArnauClavijos2004@ginebro.cat>
2023-12-19 15:11:43 +0100

    primera part seleccio acabada al 99%

--- Escenas/GridContainer.gd
+++ Escenas/GridContainer.gd
@@ -17,14 +17,15 @@ var maxPos = 0
 func _ready():
 	for i in get_children():
 		maxPos += 1
-		rect_pivot_offset.x = 32
-		rect_pivot_offset.y = 32
+		
+	player1_position = 0
+	player2_position = 1
 		
 func _input(event):
 	
 	var player1 = get_parent().get_node("player1")
 	var player2 = get_parent().get_node("player2")
-	
+
 	if (event.is_action_pressed("ui_left2")):
 		tempPos1 = player1_position - 1
 		if player1_position <= 0:
@@ -32,49 +33,38 @@ func _input(event):
 		else:
 			if tempPos1 == player2_position:
 				player1_position -= 2
-				print(player1_position)
 			else:
 				player1_position -= 1
-				print(player1_position)
-	
-	elif (event.is_action_pressed("ui_right2")):
+
+	if (event.is_action_pressed("ui_right2")):
 		tempPos1 = player1_position + 1
-		if player1_position >= maxPos:
-			player1_position = maxPos
-			print(player1_position)
+		if player1_position >= maxPos - 1:
+			player1_position = maxPos - 1
 		else:
 			if tempPos1 == player2_position:
 				player1_position += 2
-				print(player1_position)
-
 			else:
 				player1_position += 1
-				print(player1_position)
 
 	if (event.is_action_pressed("ui_left")):
 		tempPos2 = player2_position - 1
 		if player2_position <= 0:
-			player2_position = player2_position
+			player2_position = 0
 		else:
 			if tempPos2 == player1_position:
 				player2_position -= 2
-				print(player2_position)
-
 			else:
 				player2_position -= 1
-				print(player2_position)
 
 	elif (event.is_action_pressed("ui_right")):
 		tempPos2 = player2_position + 1
-		if player2_position >= maxPos:
-			player2_position = player2_position
+		if player2_position >= maxPos - 1:
+			player2_position = maxPos - 1
 		else:	
 			if tempPos2 == player1_position:
 				player2_position += 2
-				print(player2_position)
 			else:
 				player2_position += 1
-				print(player2_position)
 	
 	if (event.is_action_pressed("ui_select")):
 		selected_char1 = get_child(player1_position)
@@ -86,7 +76,7 @@ func _input(event):
 
 	# Update the positions of the texture rects
 	var texture_rect_1 = get_child(player1_position)
-	player1.global_position = texture_rect_1.get_global_position()
+	player1.global_position = texture_rect_1.get_global_position() + texture_rect_1.rect_size / 2
 
 	var texture_rect_2 = get_child(player2_position)
-	player2.global_position = texture_rect_2.get_global_position()
+	player2.global_position = texture_rect_2.get_global_position() + texture_rect_2.rect_size / 2
