e589bd4bbf1bc5ec08779ffa237471e6f3a9bb87
arnau clavijos roger <ArnauClavijos2004@ginebro.cat>
2024-02-12 11:29:28 +0100

    poc important

--- Escenas/mapSelector.gd
+++ Escenas/mapSelector.gd
@@ -1,19 +1,10 @@
 extends GridContainer
 
-onready var character_select_vars = get_node("res://Escenas/CharacterSelector.tscn")
-
-onready var selected1 = get_node("SelectedChar1")
-onready var selected2 = get_node("SelectedChar2")
-
-var selected_map1 = null
-var selected_map2 = null
-
+var selected_map = null
 
 var tempPos1 = 0
-var tempPos2 = 0
 
-var player1_position = 0
-var player2_position = 0
+onready var selector_position = 1
 
 var maxPos = 0
 
@@ -21,65 +12,26 @@ func _ready():
 	for i in get_children():
 		maxPos += 1
 		
-	player1_position = 0
-	player2_position = 1
+	selector_position = 0
 		
 func _input(event):
 
-	var player1 = get_node("player1")
-	var player2 = get_node("player2")
+	var selector = get_parent().get_node("player1")
 
 	if (event.is_action_pressed("ui_left2")):
-		tempPos1 = player1_position - 1
-		if player1_position <= 0:
-			player1_position = 0
-		else:
-			if tempPos1 == player2_position:
-				player1_position -= 2
-			else:
-				player1_position -= 1
 
+		if selector_position <= 0:
+			selector_position = 0
+			
 	if (event.is_action_pressed("ui_right2")):
-		tempPos1 = player1_position + 1
-		if player1_position >= maxPos - 1:
-			player1_position = maxPos - 1
-		else:
-			if tempPos1 == player2_position:
-				player1_position += 2
-			else:
-				player1_position += 1
 
-	if (event.is_action_pressed("ui_left")):
-		tempPos2 = player2_position - 1
-		if player2_position <= 0:
-			player2_position = 0
-		else:
-			if tempPos2 == player1_position:
-				player2_position -= 2
-			else:
-				player2_position -= 1
-
-	elif (event.is_action_pressed("ui_right")):
-		tempPos2 = player2_position + 1
-		if player2_position >= maxPos - 1:
-			player2_position = maxPos - 1
-		else:	
-			if tempPos2 == player1_position:
-				player2_position += 2
-			else:
-				player2_position += 1
-	
-	if (event.is_action_pressed("ui_select")):
-		selected_map1 = get_child(player1_position)
-		emit_signal("selectedMap1", selected_map1)
+		if selector_position >= maxPos - 1:
+			selector_position = maxPos - 1
 	
 	elif (event.is_action_pressed("ui_select2")):
-		selected_map2 = get_child(player2_position)
-		emit_signal("selectedMap2", selected_map2)
+		selected_map = get_child(selector_position)
+		emit_signal("selectedMap", selected_map)
 
 	# Update the positions of the texture rects
-	var texture_rect_1 = get_child(player1_position)
-	player1.global_position = texture_rect_1.get_global_position() + texture_rect_1.rect_size / 2
-
-	var texture_rect_2 = get_child(player2_position)
-	player2.global_position = texture_rect_2.get_global_position() + texture_rect_2.rect_size / 2
+	var texture_rect_1 = get_child(selector_position)
+	selector.global_position = texture_rect_1.get_global_position() + texture_rect_1.rect_size / 2
